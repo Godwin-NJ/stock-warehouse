8622d9c2f8774dd1118f2c0681b8d1de741d2b47
godwin-nj <amadigodwin7@gmail.com>
2023-11-16 16:10:41 +0100

    apply db-context to receipt generation

--- Services/StockReceiptService.cs
+++ Services/StockReceiptService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using stock_to_inventoryy.Data;
 using stock_to_inventoryy.Dtos.OrderReceipt;
 using stock_to_inventoryy.Models;
 
@@ -7,9 +8,11 @@ namespace stock_to_inventoryy.Services
     public class StockReceiptService : IStockReceipt
     {
         private readonly IMapper _mapper;
-        public StockReceiptService(IMapper mapper) 
+        private readonly DataContext _context; //this will initiate usage of database
+        public StockReceiptService(IMapper mapper, DataContext context) 
         { 
             _mapper = mapper;
+            _context = context;
         }
 
         public static List<StockReceipt> newStockReceipts = new List<StockReceipt>
@@ -42,16 +45,21 @@ namespace stock_to_inventoryy.Services
         {
             var service_response = new ServiceResponse<List<stockReceiptDto>>();
             var inv = _mapper.Map<StockReceipt>(stockInvoicedto);
-            inv.Id = newStockReceipts.Max(x => x.Id ) + 1;
-            newStockReceipts.Add(inv);
-            service_response.Data = newStockReceipts.Select(c => _mapper.Map<stockReceiptDto>(c)).ToList();
+         //   inv.Id = newStockReceipts.Max(x => x.Id ) + 1;
+          //  newStockReceipts.Add(inv);
+            await _context.stockReceipts.AddAsync(inv);
+            _context.SaveChanges();
+            //  service_response.Data = newStockReceipts.Select(c => _mapper.Map<stockReceiptDto>(c)).ToList();
+            service_response.Data = _context.stockReceipts.Select(c => _mapper.Map<stockReceiptDto>(c)).ToList();
             return service_response;
         }
 
         public async Task<ServiceResponse<List<stockReceiptDto>>> GetAllStockReceipt()
         {
             var service_response = new ServiceResponse<List<stockReceiptDto>>();
-            service_response.Data = newStockReceipts.Select(c => _mapper.Map<stockReceiptDto>(c)).ToList();
+            var dbData = await _context.stockReceipts.ToListAsync();
+            // service_response.Data = newStockReceipts.Select(c => _mapper.Map<stockReceiptDto>(c)).ToList();
+            service_response.Data = dbData.Select(c => _mapper.Map<stockReceiptDto>(c)).ToList();
             return service_response;
 
         }
@@ -59,7 +67,7 @@ namespace stock_to_inventoryy.Services
         public async Task<ServiceResponse<stockReceiptDto>> GetStockReceipt(int id)
         {
             var service_response = new ServiceResponse<stockReceiptDto>();
-            var getInv = newStockReceipts.FirstOrDefault(x => x.Id == id);
+            var getInv = await _context.stockReceipts.FirstOrDefaultAsync(x => x.Id == id);
             service_response.Data = _mapper.Map<stockReceiptDto>(getInv);
             return service_response;
         }
